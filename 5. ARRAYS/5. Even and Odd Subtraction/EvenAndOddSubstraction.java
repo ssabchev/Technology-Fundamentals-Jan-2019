456c66db6491fa20fcbeec67e8516f5a7cda0e46
Steliyan Sabchev <ssabchev87@gmail.com>
2019-04-26 07:09:20 +0100

    Even and Odd Subtraction

---
+++ 5. ARRAYS/5. Even and Odd Subtraction/EvenAndOddSubstraction.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class EvenAndOddSubstraction {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                System.in
+                        )
+                );
+
+        int[] data = Arrays.stream(reader.readLine().split("\\s+"))
+                    .mapToInt(e -> Integer.parseInt(e)).toArray();
+
+        int evenSum = 0;
+        int oddSum = 0;
+        for (int num : data) {
+            if (num % 2 == 0) {
+                evenSum += num;
+            } else {
+                oddSum += num;
+            }
+        }
+        System.out.println(evenSum - oddSum);
+    }
+}
